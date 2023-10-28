934c7ffd02ad691d88a9926d11da22f8cf4fc1cf
Max Xu <maxxu123456@gmail.com>
2023-10-27 20:53:18 -0700

    added basic structure for Home Component

---
+++ src/components/Home/Home.js
@@ -0,0 +1,18 @@
+import "./Home.css";
+function Home() {
+  return (
+    <div>
+      <h1>Hallmate Helper</h1>
+      <h2>What we do:</h2>
+      <p>
+        We Provide a platform where people in a dorm can make requests that are
+        specific to a specific part of dorm, simplifying the process of helping
+        your hallmates.
+      </p>
+      <button>Sign Up</button>
+      <button>Log In</button>
+    </div>
+  );
+}
+
+export default Home;
